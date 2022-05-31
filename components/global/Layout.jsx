80ab0c3d7bf08f93833f0d2cf481375248ef96b5
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-31 23:38:28 +0530

    [FEAT]: added header links

--- components/global/Layout.jsx
+++ components/global/Layout.jsx
@@ -1,6 +1,15 @@
 import Head from "next/head";
 import React from "react";
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import NextLink from "next/link";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+  IconButton,
+} from "@mui/material";
 
 const Layout = ({ children }) => {
   return (
@@ -14,12 +23,36 @@ const Layout = ({ children }) => {
           backgroundColor: "#203040",
           "& a": {
             color: "#ffffff",
-            marginLeft: 10,
           },
         }}
       >
-        <Toolbar>
-          <Typography> instaBuy </Typography>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            maxWidth: "1024px",
+            margin: "auto",
+            width: "100%",
+          }}
+        >
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography sx={{ fontWeight: "bold", fontSize: "1.5rem" }}>
+                instaBuy
+              </Typography>
+            </Link>
+          </NextLink>
+          <div sx={{ flexGrow: "1" }} />
+          <div>
+            <NextLink href="/cart" passHref>
+              <IconButton color="primary" aria-label="cart">
+                <ShoppingCartOutlinedIcon />
+              </IconButton>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
 
@@ -30,6 +63,7 @@ const Layout = ({ children }) => {
       >
         {children}
       </Container>
+
       <footer sx={{ marginTop: 10, textAlign: "center" }}>
         <Typography> All rights reserved. instaBuy </Typography>
       </footer>
