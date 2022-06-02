f7e2b0cb668ab5c66482ef8d72c284e7ecc5752e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-06-03 01:01:17 +0530

    [FEAT]: register api

---
+++ pages/api/users/register.js
@@ -0,0 +1,29 @@
+import nc from "next-connect";
+import bcrypt from "bcryptjs";
+import { db, signToken } from "../../../utils";
+import User from "../../../model/user";
+
+const handler = nc();
+
+handler.post(async (req, res) => {
+  await db.connect();
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password),
+    isAdmin: false,
+  });
+  const user = await newUser.save();
+  await db.disconnect();
+
+  const token = signToken(user);
+  res.send({
+    token,
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  });
+});
+
+export default handler;
--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,7 @@
-/* eslint-disable react/no-unescaped-entities */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import NextLink from "next/link";
 import { useRouter } from "next/router";
+import NextLink from "next/link";
 import Cookies from "js-cookie";
 import {
   Button,
@@ -12,15 +11,12 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { Layout } from "../components";
 import { useContextState } from "../context/StateProvider";
+import { Layout } from "../components";
 
-export default function Login() {
+export default function Register() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const { redirect } = router.query; // login?redirect=/shipping
+  const { redirect } = router.query;
 
   const { state, stateDispatch } = useContextState();
 
@@ -32,17 +28,31 @@ export default function Login() {
     }
   }, []);
 
+  const [name, setName] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const submitHandler = async (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      // eslint-disable-next-line no-alert
+      alert("passwords don't match");
+      return;
+    }
     try {
-      const { data } = await axios.post("/api/users/login", {
+      const { data } = await axios.post("/api/users/register", {
+        name,
         email,
         password,
       });
 
       stateDispatch({ type: "USER_LOGIN", payload: data });
 
-      Cookies.set("userInfo", JSON.stringify(data), { sameSite: "strict" });
+      Cookies.set("userInfo", data, { sameSite: "strict" });
 
       router.push(redirect || "/");
     } catch (err) {
@@ -50,17 +60,26 @@ export default function Login() {
       alert(err.response.data ? err.response.data.message : err.message);
     }
   };
-
   return (
-    <Layout title="Login">
+    <Layout title="Register">
       <form
-        onSubmit={submitHandler}
         style={{ maxWidth: 800, margin: "0 auto" }}
+        onSubmit={submitHandler}
       >
-        <Typography sx={{ textAlign: "center" }} component="h1" variant="h1">
-          Login
+        <Typography component="h1" variant="h1">
+          Register
         </Typography>
         <List>
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="name"
+              label="Name"
+              inputProps={{ type: "text" }}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </ListItem>
           <ListItem>
             <TextField
               variant="outlined"
@@ -82,14 +101,24 @@ export default function Login() {
             />
           </ListItem>
           <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="confirmPassword"
+              label="Confirm Password"
+              inputProps={{ type: "password" }}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </ListItem>
+          <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
-              Login
+              Register
             </Button>
           </ListItem>
           <ListItem>
-            Don't have an account? &nbsp;
-            <NextLink href={`/register?redirect=${redirect || "/"}`} passHref>
-              <Link>Register</Link>
+            Already have an account? &nbsp;
+            <NextLink href={`/login?redirect=${redirect || "/"}`} passHref>
+              <Link>Login</Link>
             </NextLink>
           </ListItem>
         </List>
