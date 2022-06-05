27fc184172c1443b3ce3abc7c2b1f0e5e4741d22
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-06-05 14:02:49 +0530

    [FIX]: preventing seeding in prod

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,18 +1,18 @@
 import nc from "next-connect";
 
-import Product from "../../model/product";
-import User from "../../model/user";
-import { data, db } from "../../utils";
+// import Product from "../../model/product";
+// import User from "../../model/user";
+// import { data, db } from "../../utils";
 
 const handler = nc();
 
 handler.get(async (req, res) => {
-  await db.connect();
-  await User.deleteMany();
-  await User.insertMany(data.users);
-  await Product.deleteMany();
-  await Product.insertMany(data.products);
-  await db.disconnect();
+  // await db.connect();
+  // await User.deleteMany();
+  // await User.insertMany(data.users);
+  // await Product.deleteMany();
+  // await Product.insertMany(data.products);
+  // await db.disconnect();
   res.send({ message: "seeded successfully" });
 });
 
