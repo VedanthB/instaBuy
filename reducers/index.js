cc117debdd0af88c8b733870f92a7f9422bb166b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-10-31 20:43:52 +0530

    [FEAT]: refactored admin users page

--- reducers/index.js
+++ reducers/index.js
@@ -4,3 +4,5 @@ export { adminDashboardReducer } from "./adminDashboardReducer";
 export { adminOrderReducer } from "./adminOrderReducer";
 export { adminProductsReducer } from "./adminProductsReducer";
 export { adminEditProductReducer } from "./adminEditProductReducer";
+export { adminUsersReducer } from "./adminUsersReducer";
+export { adminEditUserReducer } from "./adminEditUserReducer";
