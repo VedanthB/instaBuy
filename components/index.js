09e5424a85db39d300cd111a5b919e4e138e2102
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-10-31 16:50:25 +0530

    [FEAT]: refactored cart page

--- components/index.js
+++ components/index.js
@@ -4,5 +4,7 @@ import ProductItem from "./global/ProductItem";
 export { Layout } from "./global/Layout";
 export { ProductDetails } from "./product/ProductDetails";
 export { ProductReviews } from "./product/ProductReviews";
+export { CheckoutCard } from "./cart/CheckoutCard";
+export { CartItemsTable } from "./cart/CartItemsTable";
 
 export { CheckoutWizard, ProductItem };
--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -1,39 +1,22 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable radix */
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NextLink from "next/link";
-import axios from "axios";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import {
-  Button,
-  Card,
-  CircularProgress,
-  Grid,
-  Link,
-  List,
-  ListItem,
-  Rating,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useSnackbar } from "notistack";
+
+import { Typography } from "@mui/material";
+
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { Layout, ProductDetails, ProductReviews } from "../../components";
-import { db, getError } from "../../utils";
+import { db } from "../../utils";
 import Product from "../../model/product";
-import { useContextState } from "../../context/StateProvider";
 
 export default function ProductScreen({ product }) {
-  const router = useRouter();
-
   if (!product) {
     return <div>Product Not Found</div>;
   }
 
   return (
     <Layout title={product.name} description={product.description}>
-      <div sx={{ marginTop: 10, marginBottom: 10 }}>
+      <div>
         <NextLink href="/" passHref>
           <Typography
             sx={{
@@ -44,6 +27,7 @@ export default function ProductScreen({ product }) {
               "&:hover": {
                 textDecoration: "underline",
               },
+              marginBottom: 10,
             }}
           >
             <ChevronLeftIcon sx={{ cursor: "pointer", fontSize: "2rem" }} />
